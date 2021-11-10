c2d05e6a8e05111cb04c1a7e3f2fffb0bfbe4fe5
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2021-11-10 21:49:43 +0100

    [feat] add grade converter component

---
+++ src/components/GradeSelector.svelte
@@ -0,0 +1,42 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import { gradeSystem } from '../store';
+
+  let selected;
+
+  const gradeSystems = [
+    { name: 'same as gym', value: undefined },
+    { name: 'uiaa', value: 'uiaa' },
+    { name: 'yds', value: 'yds' },
+    { name: 'French', value: 'french' },
+    { name: 'French boulder', value: 'french_boulder' },
+    { name: 'French rounded', value: 'french_rounded' },
+    { name: 'ewbank', value: 'ewbank' },
+    { name: 'British', value: 'british' },
+    { name: 'V Grade', value: 'v_grade' },
+  ];
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="p-4">
+  <label class="mb-1 block font-bold" for="gradeSystem">Grade system</label>
+  <select
+    class="px-2 py-0.5 border-2 rounded w-full"
+    name=""
+    id="gradeSystem"
+    bind:value={selected}
+    on:change={() => {
+      gradeSystem.set(selected);
+
+      dispatch('change', selected);
+    }}
+  >
+    {#each gradeSystems as grade}
+      <option value={grade.value}>{grade.name}</option>
+    {/each}
+  </select>
+</div>
+
+<style>
+</style>
